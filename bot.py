cbba0140d877f1153ae9f9bd2af7095a92b057b3
chariyew <cchariyew11@gmail.com>
2025-12-31 18:52:56 +0500

    Change environment variable from TELEGRAM_TOKEN to BOT_TOKEN

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 
-TOKEN = os.getenv("TELEGRAM_TOKEN")
+TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Бот работает 24/7 🚀")
