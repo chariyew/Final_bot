371f169493a99289b1b32ac93a5a2b3ef4eb9e6a
chariyew <cchariyew11@gmail.com>
2026-01-01 21:45:16 +0500

    Update bot token retrieval method

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("7981684997:AAFVXRFA2awTtdfhURJSf-CVLoKmrqWrNDM")
+TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Бот работает, брат 💪")
