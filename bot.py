3858c22f3d14ecfa42ac15537ac9872075cb0d46
chariyew <cchariyew11@gmail.com>
2026-01-04 13:56:53 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from telegram.ext import (
 )
 
 # ================= НАСТРОЙКИ =================
-TELEGRAM_TOKEN = "PASTE_YOUR_REAL_TOKEN_HERE"
+TELEGRAM_TOKEN = "7981684997:AAG2T3gCoJbfE2X6qDd2S0AzE53C65MKCsg"
 CHANNEL_CHAT_ID = -1003531475408   # твой канал
 ADMIN_ID = 8039171205
 
